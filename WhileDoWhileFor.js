16ac7405dd27889400272fee256b32b025026933
Yasin Beken <yasinbeken16@gmail.com>
2024-03-01 15:49:25 +0300

    First Commit

---
+++ WhileDoWhileFor.js
@@ -0,0 +1,46 @@
+// WHİLE DÖNGÜSÜ
+
+// while(Dongunun çalışma koşulı){
+//     dongu çalıştığı sürece çalışacak kod
+// }
+
+
+// BİRDEN 10 a KADAR YAZIM
+// let i=0
+// while(i<10){
+//     console.log(i)
+//     i++
+// }
+
+//DO WHİLE
+
+// do{
+//     çalışacak kod
+// }while(koşul)
+
+// let i=0
+// do{
+//     i++
+//     console.log(i)
+// }while(i<10)
+
+// let liste=["Ahmet","Mehmet","Muratcan","Yasin","Umut","Osman","Davut"]
+// // liste.forEach(x=>console.log(x))
+// for(let a in liste){
+//     console.log(liste[a])
+// }
+
+//Çarpım tablosu oluşturun döngüler ile
+// document.write("<table border='1'><tr>")
+// let carpim=1
+// for(let i=1;i<=10;i++){
+//     document.write("<td>")
+//     for(let j=1;j<=10;j++){
+//         document.write(i+" x "+j+" = "+i*j+"<br>")
+//     }
+//     document.write("</td>")
+//     if(i==5){
+//         document.write("</tr><tr>")
+//     }
+// }
+// document.write("</tr></table>
